38a70ea448e5acc86c395fa95708d3e1ee1414a7
Sudhakar <sudhakar@gmail.com>
2022-04-06 11:04:02 +0530

    less Iterations to find PrimeNumber

--- primeNumber.rb
+++ primeNumber.rb
@@ -1,16 +1,13 @@
 puts "Enter the Number:"
 num = gets.to_i
+temp = Integer(num/2)
 count=0
-if (num ==0)
-	puts "0 is Not a Prime Number"
-else
-	i=2
-	while (i<=num)
-		if (num%i==0)
+i=2
+while (i<=num)
+	if (num%i==0)
 			count+=1	
-		end
-		i+=1
 	end
+		i+=1
 end
 	if (count>1)
 		puts "#{num} is not a Prime Number"
